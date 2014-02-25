8cb45bafe146148fde1fd478da33b473b0d10653
Ross MacFarlane <rsmacfarlane88@gmail.com>
2014-02-25 22:14:06 +0000

    Changed folder name and ignored some files

---
+++ NewsApp/Feed.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsApp
+{
+    public class Feed
+    {
+        public string Name { get; set; }
+        public Uri FeedUri { get; set; }
+        public FeedCategory Category { get; set; }
+        public string Publisher { get; set; }
+
+    }
+
+    public enum FeedCategory
+    {
+        News,
+        Sport,
+        Programming,
+        Finance,
+        Technology,
+        Other
+    }
+}
